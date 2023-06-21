761471b6306534c3cd070cae24bc32eb67c03641
li lei <ruojianll@gmail.com>
2023-06-21 17:39:12 +0800

    a

--- src/decorator/bind.ts
+++ src/decorator/bind.ts
@@ -1,6 +1,5 @@
 import { initPrototypeSlot } from './propertySlot'
 export function Bind(proto:any,name:string){
-    console.log(name,proto)
     const slot = initPrototypeSlot(proto)
     slot.bindKeys ??= new Set
     slot.bindKeys.add(name)
